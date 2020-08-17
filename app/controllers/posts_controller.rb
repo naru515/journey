029c662b745645db82e83a13544f1ac46700a233
naru515 <naruyn0515@yahoo.co.jp>
2020-08-17 22:53:28 +0900

    08/17

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-  end   
+  end
+
+  def new
+    # @post = Posts.new
+  end 
+  
+  def create
+    Post.create(post_params)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :image, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: :index
+  root to: 'posts#index'
+  resources :posts, only: [:index, :new, :create, :destroy]
 end
+ 
